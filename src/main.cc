ce73e718f805fc5324a5107d72f7d5e850fa9db5
Oscar Campbell <oscar@campbell.nu>
2018-08-14 09:47:44 +0200

    way late commit

--- src/main.cc
+++ src/main.cc
@@ -19,15 +19,15 @@ let const LOAD_CURRENT_PIN = PinNumber(0);
 let const REG_VOLT_PIN = PinNumber(1);
 let const PV_CURRENT_PIN = PinNumber(2);
 
-let ledCurrentEma =  Dsp::Ema<F32, false>(1000);
+let ledCurrentEma = Dsp::Ema<F32, false>(1000);
 let pvCurrentEma =  Dsp::Ema<F32, false>(1000);
 /* let slowEma =     Dsp::Ema<F32, false>(14); */
 let samples =     0UL;
 
 fn readLedCurrent() -> F32 {
-   let const bias_calib = 270;//-111.0; // Still gives about -5ma error biase, but one step up gives +30mA error - when resolving here at the int-level
-   let const scale_calib = 1.433;
+   let const bias_calib = 270;   //-111.0; // Still gives about -5ma error bias, but one step up gives +30mA error - when resolving here at the int-level
    let const mVPerAmp = 185;
+   let const scale_calib = 1.433;
    let const mVRef = 5000.0;
    let const mA_p_V_p_U = (mVRef / 1023.0) / mVPerAmp * scale_calib * 1000;
    let const bias = (-511 * mA_p_V_p_U) + bias_calib;
@@ -40,9 +40,9 @@ fn readLedCurrent() -> F32 {
 }
 
 fn readPVCurrent() -> F32 {
-   let const bias_calib = 260;//-111.0; // Still gives about -5ma error biase, but one step up gives +30mA error - when resolving here at the int-level
-   let const scale_calib = 1.433;
+   let const bias_calib = 260;   //-111.0; // Still gives about -5ma error bias, but one step up gives +30mA error - when resolving here at the int-level
    let const mVPerAmp = 50;
+   let const scale_calib = 1.433;
    let const mVRef = 5000.0;
    let const mA_p_V_p_U = (mVRef / 1023.0) / mVPerAmp * scale_calib * 1000;
    let const bias = (-511 * mA_p_V_p_U) + bias_calib;
@@ -80,6 +80,7 @@ fn setup_programmable_voltage() -> Void {
 fn prime_current_emas() -> Void {
    ledCurrentEma.prime(readLedCurrent());
    pvCurrentEma.prime(readPVCurrent());
+
    for(let i = 5000; i != 0; --i) {
       ledCurrentEma << readLedCurrent();
       pvCurrentEma << readPVCurrent();
@@ -88,15 +89,15 @@ fn prime_current_emas() -> Void {
 }
 
 fn setup() -> Void {
-   Serial.begin(115200);            // Initialize serial communications with the PC
+   Serial.begin(115200);      // Initialize serial communications with the PC
    say("Kick Off!\n\r");
 
    setup_programmable_voltage();
    prime_current_emas();
 }
 
-/* let const TOP = 99; */
-let const TOP = 25;
+/* let const COUNTER_TOP = 99; */
+let const COUNTER_TOP = 25;
 let const LOOP_DELAY = 1000; //300
 
 fn loop() -> Void {
@@ -144,12 +145,13 @@ fn loop() -> Void {
       say("\n\r");
 
       r_counter += r_dir;
-      if (r_counter >= TOP) {
+
+      if (r_counter == COUNTER_TOP) {
          dioWrite<3>(0);
          r_dir = -1;
          say("\n\rtop reached\n\r\n\r");
 
-      } else if (r_counter <= 0) {
+      } else if (r_counter == 0) {
          dioWrite<3>(1);
          r_dir = 1;
          say("\n\rbottom reached\n\r\n\r");
